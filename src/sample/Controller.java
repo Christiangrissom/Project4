e812a8b7575557fabc166d5000f76820692dce3e
Christian <christiangrissom009@gmail.com>
2020-12-07 20:48:16 -0600

    Giving up on controlling it through controller
    Controller is too difficult to work with, so for now it is going to be ignored

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,7 +2,29 @@ package sample;
 
 import javafx.event.ActionEvent;
 
+
 public class Controller {
 
+    static int sliderValue;
+
+
+    public Controller(){
+
+    sliderValue = 0;
+}
+
+    public void setSlider(int slider){
+        System.out.println("Slider's value is :" + slider);
+        sliderValue = slider;
+        checkSlider();
+    }
+    public void checkSlider(){
+        System.out.println("Checkslider value is: " + sliderValue);
+        Main m = new Main();
+        m.HammDist_Field.setEditable(true);
+        m.HammDist_Field.setText(String.valueOf(sliderValue));
+        m.HammDist_Field.setEditable(false);
+
+    }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,49 +1,66 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-
-    Integer Hamm_Dist = 1;
+    Label HammDist_Label;
     Button Show_Station;
     TextField HammDist_Field;
-    Label HammDist_Label;
     Insets gridPadding;
+    Slider slider;
+    GridPane layout;
+    Controller c = new Controller();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Hamming Distance");
 
-        GridPane layout = new GridPane();
+        layout = new GridPane();
         layout.setAlignment(Pos.TOP_LEFT);
 
 
 
-        HammDist_Label = new Label(); 
+        HammDist_Label = new Label();
         HammDist_Label.setText("Enter Hamming Dist:");
+        Font font = Font.font("Verdana", FontWeight.BOLD, 12);
+        HammDist_Label.setFont(font);
         layout.add(HammDist_Label, 0, 0);
+
+
+
+        slider = new Slider(1,4,1 );
+        slider.setSnapToTicks(true);
+        slider.setMajorTickUnit(4);
+        slider.setShowTickMarks(true);
+        slider.setShowTickLabels(true);
+        slider.setValueChanging(true);
+        layout.add(slider, 0,2);
 
         HammDist_Field = new TextField();
         HammDist_Field.setEditable(false);
         HammDist_Field.setPrefColumnCount(15);
-        HammDist_Field.setText(Integer.toString(Hamm_Dist));
+
+        HammDist_Field.setText("9");
         layout.add(HammDist_Field, 1, 0);
 
         Show_Station = new Button();
         Show_Station.setText("Do not click me");
         Show_Station.setOnAction(e->System.out.println("WOW! It worked!"));
         //layout.add(Show_Station);
-
-
-
 
         gridPadding = new Insets(10,10,10,10);
         layout.setPadding(gridPadding);
@@ -59,7 +76,7 @@ public class Main extends Application{
 
 
     public static void main(String[] args) {
-        launch(args);
+            launch(args);
     }
 
 }
