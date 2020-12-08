2a20479cb4709df14142c50743e96faa5e5f9215
Christian <christiangrissom009@gmail.com>
2020-12-07 23:11:00 -0600

    The listView and button is almost functional

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,21 +3,23 @@ package sample;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 
 public class Main extends Application{
+    ListView StationList;
     Label HammDist_Label;
     Button Show_Station;
     TextField HammDist_Field;
@@ -70,8 +72,26 @@ public class Main extends Application{
 
         Show_Station = new Button();
         Show_Station.setText("Show Station");
-        Show_Station.setOnAction(e->System.out.println("WOW! It worked!"));
+        Show_Station.setOnAction(actionEvent -> {//change this to work for the next part of the code
+            if(true){
+            System.out.println("The button works, and so does the if statement.");
+            }
+        });
         layout.add(Show_Station,0,3);
+
+
+        ArrayList<String> al = new ArrayList();//use a method or a class to simplify this
+        al.add("STA1");
+        al.add("STA2");
+        al.add("STA3");
+        al.add("STA4");
+        al.remove("STA4");
+
+        ObservableList<String> names = FXCollections.observableArrayList(al);
+        StationList = new ListView<String>(names);
+        StationList.setEditable(false);
+        layout.add(StationList,0,4);
+
 
         gridPadding = new Insets(10,10,10,10);
         layout.setPadding(gridPadding);
